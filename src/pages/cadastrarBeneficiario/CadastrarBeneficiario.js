9b68c15c4de8ac17f4ee16e3ed4e2a921f01c509
Rosenato Barreto <rosenatoblima@gmail.com>
2022-12-26 15:53:01 -0300

    Update CadastrarBeneficiario.js
    Atualizando cadastrar beneficiário

--- src/pages/cadastrarBeneficiario/CadastrarBeneficiario.js
+++ src/pages/cadastrarBeneficiario/CadastrarBeneficiario.js
@@ -195,11 +195,11 @@ class CreateUser extends React.Component {
                             </div>
 
                             <div className="col-span-6 sm:col-span-3">
-                              <label for="country" className="block text-sm font-medium text-gray-700">Country</label>
-                              <select id="country" name="country" autocomplete="country-name" className="mt-1 block w-full rounded-md border border-green-300 bg-white py-2 px-3 shadow-sm focus:border-green-500 focus:outline-none focus:ring-green-500 sm:text-sm">
-                                <option>United States</option>
-                                <option>Canada</option>
-                                <option>Mexico</option>
+                              <label for="campus" className="block text-sm font-medium text-gray-700">Campus</label>
+                              <select id="campus" name="campus" autocomplete="campus-name" className="mt-1 block w-full rounded-md border border-green-300 bg-white py-2 px-3 shadow-sm focus:border-green-500 focus:outline-none focus:ring-green-500 sm:text-sm">
+                                <option>Monteiro</option>
+                                {/* <option>Cabedelo</option>
+                                <option>Campina Grande</option> */}
                               </select>
                             </div>
 
